4b50b3ab527b055be51ba0460d285b010a2613e3
angel-torres <alas4224@gmail.com>
2020-04-20 13:24:46 -0500

    added material ui select

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,41 @@ import './App.css';
 
 import { Typography, Button, TextField } from '@material-ui/core';
 
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
 // COMPONENTS
 import Results from './components/Results';
 
+const searchCategories = [
+'anime', 
+'manga', 
+'person', 
+'character',
+'genre'
+]
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }),
+);
+
 const App: React.FC<{}> = () => {
   const [ input, setInput ] = useState<string>("");
+  const [ category, setCategory ] = useState<string>("");
   const [ results, setResults ] = useState<[]>([]);
+  const classes = useStyles();
 
   const handleChanges = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
@@ -26,12 +55,33 @@ const App: React.FC<{}> = () => {
     })
   }
 
+  const handleSelect = (e: React.ChangeEvent<{value: unknown}>) => {
+
+  }
+
   return (
     <div className="App">
       <header>
         <Typography style={{float: "left"}} variant="h2" component="h1">Anime Finder</Typography>
         <div>
           <form onSubmit={handleSubmit}>
+          <FormControl variant="filled" className={classes.formControl}>
+              <InputLabel id="demo-simple-select-filled-label">Category</InputLabel>
+              <Select
+                labelId="demo-simple-select-filled-label"
+                id="demo-simple-select-filled"
+                value={category}
+                onChange={handleSelect}
+                variant="outlined"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
             <TextField 
               variant="outlined" 
               onChange={handleChanges} 
