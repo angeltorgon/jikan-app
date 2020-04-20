24bec692d21e0bb0817600cd8a4f25025cc50fa9
angel-torres <alas4224@gmail.com>
2020-04-20 14:18:28 -0500

    was able to get search working with different categories

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,29 +14,34 @@ import Select from '@material-ui/core/Select';
 // COMPONENTS
 import Results from './components/Results';
 
-const searchCategories = [
-'anime', 
-'manga', 
-'person', 
-'character',
-'genre'
-]
-
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     formControl: {
       margin: theme.spacing(1),
       minWidth: 120,
+      width: "600px",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-around",
     },
     selectEmpty: {
       marginTop: theme.spacing(2),
     },
+    select: {
+      width: "70%"
+    },
+    search: {
+      width: "100%"
+    },
+    button: {
+      width: "70%"
+    }
   }),
 );
 
 const App: React.FC<{}> = () => {
   const [ input, setInput ] = useState<string>("");
-  const [ category, setCategory ] = useState<string>("");
+  const [ category, setCategory ] = useState("");
   const [ results, setResults ] = useState<[]>([]);
   const classes = useStyles();
 
@@ -46,7 +51,9 @@ const App: React.FC<{}> = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    axios.get(`https://api.jikan.moe/v3/search/anime?q=${input}&page=1`)
+    const requestURL = `https://api.jikan.moe/v3/search/${category}?q=${input}&page=1`
+    console.log(requestURL)
+    axios.get(`https://api.jikan.moe/v3/search/${category}?q=${input}&page=1`)
     .then((res) => {
       console.log(res.data.results)
       setResults(res.data.results);
@@ -55,42 +62,43 @@ const App: React.FC<{}> = () => {
     })
   }
 
-  const handleSelect = (e: React.ChangeEvent<{value: unknown}>) => {
-
+  const handleSelect = (e: any) => {
+    setCategory(e.target.value);
   }
 
   return (
     <div className="App">
       <header>
-        <Typography style={{float: "left"}} variant="h2" component="h1">Anime Finder</Typography>
-        <div>
-          <form onSubmit={handleSubmit}>
-          <FormControl variant="filled" className={classes.formControl}>
-              <InputLabel id="demo-simple-select-filled-label">Category</InputLabel>
-              <Select
-                labelId="demo-simple-select-filled-label"
-                id="demo-simple-select-filled"
-                value={category}
-                onChange={handleSelect}
-                variant="outlined"
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>
+        <Typography variant="h2" component="h1">Anime Finder</Typography>
+        <form onSubmit={handleSubmit}>
+        <FormControl variant="filled" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-filled-label">Category</InputLabel>
+            <Select
+              labelId="demo-simple-select-filled-label"
+              id="demo-simple-select-filled"
+              value={category}
+              onChange={handleSelect}
+              variant="outlined"
+              className={classes.select}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={"anime"}>Anime</MenuItem>
+              <MenuItem value={"manga"}>Manga</MenuItem>
+              <MenuItem value={"person"}>Person</MenuItem>
+              <MenuItem value={"character"}>Character</MenuItem>
+            </Select>
             <TextField 
               variant="outlined" 
               onChange={handleChanges} 
               value={input} 
               label="Search"
+              className={classes.search}
               type="text" />
-            <Button variant="contained" type="submit">Search</Button>
-          </form>
-        </div>
+            <Button className={classes.button} variant="contained" type="submit">Search</Button>
+          </FormControl>
+        </form>
       </header>
       <div>
         <Results results={results}/>
