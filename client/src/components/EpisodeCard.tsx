7dc90b58c40df3269d86e7d58b8ae234a4ad7899
angel-torres <alas4224@gmail.com>
2020-04-25 19:22:02 -0500

    updated styles for episode card

--- client/src/components/EpisodeCard.tsx
+++ client/src/components/EpisodeCard.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Paper } from '@material-ui/core';
 import moment from 'moment';
 import './styles/episode-card.css';
 
@@ -8,11 +7,13 @@ const EpisodeCard: React.FC<any> = ({episode}) => {
         <div className="episode-card-container" >
             <div className="episode-wrapper">
                 <h2>{episode.title}</h2>
-                <div className="episode-info">
+                <div className="episode-info-continer">
                     <p className="episode-info">Episode: {episode.episode_id}</p>
                     <p className="episode-info">Aired: {moment(episode.aired).format("LL")}</p>
-                    <a className="card-link" href={`${episode.forum_url}`}>Visit Forum</a> 
-                    <a className="card-link" href={`${episode.video_url}`}>View Video</a> 
+                    <div className="episode-link-container">
+                        <a className="card-link" href={`${episode.forum_url}`}>Visit Forum</a> 
+                        <a className="card-link" href={`${episode.video_url}`}>View Video</a> 
+                    </div>
                 </div>
             </div>
         </div>
